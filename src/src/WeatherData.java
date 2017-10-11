a03d67dc52bfa54f89232d4a7257df7a73217c04
Karl-Hendrik <karind@ttu.ee>
2017-10-11 21:06:42 +0300

    it's lit

---
+++ src/src/WeatherData.java
@@ -0,0 +1,29 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+public class WeatherData {
+
+    public JsonObject weatherData;
+
+    public JsonObject getJsonData(String Url) throws IOException {
+        HttpURLConnection request = HttpUtility.makeHttpGetRequest(Url);
+        request.connect();
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        this.weatherData = root.getAsJsonObject();
+        request.disconnect();
+        return weatherData;
+    }
+
+    public static void main(String[] args) throws IOException {
+        WeatherData data = new WeatherData();
+        System.out.println(data.getJsonData("http://api.openweathermap.org/data/2.5/weather?q=Tallinn,ee&appid=1213b3bd7d7dd50d09ce5464347f3c71").get("main"));
+    }
+
+}
---
+++ src/unittests/UnitTests.java
@@ -0,0 +1,130 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class UnitTests {
+    private JsonObject currentWeatherData;
+    private JsonArray forecastWeatherData;
+
+    @Before
+    public void setUpWeatherObject() throws IOException{
+        currentWeatherData = new WeatherData().getJsonData("http://api.openweathermap.org/data/2.5/weather?q=Tallinn,ee&appid=1213b3bd7d7dd50d09ce5464347f3c71").getAsJsonObject();
+        forecastWeatherData = new WeatherData().getJsonData("http://api.openweathermap.org/data/2.5/forecast?q=Tallinn&APPID=1213b3bd7d7dd50d09ce5464347f3c71").get("list").getAsJsonArray();
+    }
+
+    //@Test
+    //public void testCoordinateArraySize(){
+    //    try {
+    //        assertTrue(Placeholder.getCoordinates().size() == 2);
+    //    } catch (Exception e) {
+    //        fail("Failure cause : " + e.getMessage());
+    //    }
+    //}
+
+    //@Test
+    //public void testLatitudeValidity(){
+    //    try {
+    //        double latitude = currentWeatherData.get("coord").getAsJsonObject().get("lat").getAsDouble();
+    //        assertTrue(latitude > -90 && latitude < 90);
+    //    } catch (Exception e) {
+    //        fail("Failure cause : " + e.getMessage());
+    //    }
+    //}
+
+    //@Test
+    //public void testLongitudeValidity(){
+    //    try {
+    //        double longitude = currentWeatherData.get("coord").getAsJsonObject().get("lon").getAsDouble();
+    //        assertTrue(longitude > -180 && longitude < 180);
+    //    } catch (Exception e) {
+    //        fail("Failure cause : " + e.getMessage());
+    //    }
+    //}
+
+    @Test
+    public void testCurrentTemperatureRange(){
+        try {
+            int minTemp = -273;
+            int maxTemp = 500;
+            assertTrue(currentWeatherData.get("main").getAsJsonObject().get("temp").getAsInt() >= minTemp && currentWeatherData.get("main").getAsJsonObject().get("temp").getAsInt() <= maxTemp);
+        } catch (Exception e) {
+            fail("Failure cause : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testForecastTemperatureRanges() {
+        try {
+            int minTemp = -273;
+            int maxTemp = 500;
+            for (int i = 0; i < forecastWeatherData.size(); i++) {
+                double currentTemp = forecastWeatherData.get(i).getAsJsonObject().get("main").getAsJsonObject().get("temp").getAsDouble();
+                if (!(currentTemp >= minTemp && currentTemp <= maxTemp)) { fail("Array Index " + i + "Temperature out of possible range : " + currentTemp); }
+            }
+        } catch (Exception e) {
+            fail("Failure cause : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testHumidityRange() {
+        try {
+            int minRange = 0;
+            int maxRange = 100;
+            int currentHumidity = currentWeatherData.get("main").getAsJsonObject().get("humidity").getAsInt();
+            assertTrue(currentHumidity >= minRange && currentHumidity <= maxRange);
+        } catch (Exception e) {
+            fail("Failure cause : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testForecastHumidityRanges() {
+        try {
+            int minRange = 0;
+            int maxRange = 100;
+            for (int i = 0; i < forecastWeatherData.size(); i++) {
+                int currentHumidity = forecastWeatherData.get(i).getAsJsonObject().get("main").getAsJsonObject().get("humidity").getAsInt();
+                if (!(currentHumidity >= minRange && currentHumidity <= maxRange)) { fail("Array Index " + i + " Humidity out of range : " + currentHumidity); }
+            }
+        } catch (Exception e) {
+            fail("Failure cause : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCountryCode() {
+        try {
+            String countryCode = currentWeatherData.get("sys").getAsJsonObject().get("country").getAsString();
+            assertTrue(countryCode.equals("EE"));
+        } catch (Exception e) {
+            fail("Failure cause : " + e.getMessage());
+        }
+    }
+
+    //@Test
+    //public void testCoordinatePairAsString(){
+    //    try {
+    //        String line = Placeholder.getCoordinateString();
+    //        Pattern p = Pattern.compile("(^0|^-0[^\\.])|(\\.$)");
+    //        double x = Double.parseDouble(line.split(" ")[0]);
+    //        double y = Double.parseDouble(line.split(" ")[1]);
+    //        //System.out.println(x + " " + y);
+    //        if(p.matcher(line.split(" ")[0]).find() || p.matcher(line.split(" ")[1]).find()) {
+    //            fail();
+    //        } else if(x >= -90 && x <= 90 && y >= -180 && y <= 180) {
+    //            return;
+    //        } else {
+    //            fail();
+    //        }
+    //    } catch (Exception e) {
+    //        fail("Failure cause : " + e.getMessage());
+    //    }
+    //}
+
+}
