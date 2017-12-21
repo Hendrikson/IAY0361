b7216a14b1a1974c39ebd9db1fa25fa5ae8163f5
Karl-Hendrik <karind@ttu.ee>
2017-12-21 20:10:14 +0200

    Changed API Mock tests

--- src/tests/weatherdata/WeatherDataMock.java
+++ src/tests/weatherdata/WeatherDataMock.java
@@ -17,17 +17,15 @@ import static org.mockito.Mockito.*;
 public class WeatherDataMock {
     @Test
     public void testWeatherDataCallsHttpUtilityMockFunction() {
+        String requestString = "http://api.openweathermap.org/data/2.5/weather?q=Tallinn,ee&appid=1213b3bd7d7dd50d09ce5464347f3c71";
         HttpUtility httpUtilityMock = mock(HttpUtility.class);
-        WeatherData weatherDataSpy = spy(new WeatherData(httpUtilityMock));
         try {
-            weatherDataSpy.getJsonData(""); // Always throws NullPointerException
-        } catch (NullPointerException e) {
-            try {
-                verify(weatherDataSpy, times(1)).getJsonData("");
-                verify(httpUtilityMock, times(1)).makeHttpGetRequest("");
-            } catch (IOException io) {
-                fail("Failed due to IOException");
-            }
+            when(httpUtilityMock.makeHttpGetRequest(requestString)).thenReturn((HttpURLConnection) new URL(requestString).openConnection());
+            WeatherData weatherDataSpy = spy(new WeatherData(httpUtilityMock));
+            weatherDataSpy.getJsonData(requestString);
+
+            verify(weatherDataSpy, times(1)).getJsonData(requestString);
+            verify(httpUtilityMock, times(1)).makeHttpGetRequest(requestString);
         } catch (Exception e) {
             fail("Exception caught: " + e.getMessage());
         }
